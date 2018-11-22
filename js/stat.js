656631faf55da9b2f35a0967822f7b1c39353f49
Ivan <nponou@gmail.com>
2018-11-22 03:11:54 +0300

    Исправление цикла отрисовки гистаграмм и функции получения максимального значения из массива

--- js/stat.js
+++ js/stat.js
@@ -12,52 +12,35 @@ var TEXT_Y = 260;
 var TEXT_X = 140;
 var BAR_Y = 240;
 var BAR_X = 140;
+var HEADER_X = 120;
+var HEADER_Y = 40;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-  return maxElement;
-};
-
-
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 270, 30);
-  ctx.fillText('Список результатов:', 260, 50);
+  ctx.fillText('Ура вы победили!', HEADER_X, HEADER_Y);
+  ctx.fillText('Список результатов:', HEADER_X, HEADER_Y + (CLOUD_GAP * 2));
 
-  var maxTime = getMaxElement(times);
+  var maxTime = Math.max.apply(null, times);
+  var getRandom = function (min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+  };
 
   for (var i = 0; i < names.length; i++) {
-    var getRandom = function (min, max) {
-      var rand = min - 0.5 + Math.random() * (max - min + 1);
-      rand = Math.round(rand);
-      return rand;
-    };
     var randomColor = 'rgb( 0, 0, ' + getRandom(50, 255) + ')';
-
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillText(names[i], TEXT_X + (BAR_WIDTH + GAP) * i, TEXT_Y);
-      ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -(times[i] * MAX_BAR_HEIGHT / maxTime));
-      ctx.fillText(times[i], TEXT_X + (BAR_WIDTH + GAP) * i, (BAR_Y - CLOUD_GAP - (times[i] * MAX_BAR_HEIGHT / maxTime)));
-    } else {
-      ctx.fillStyle = randomColor;
-      ctx.fillText(names[i], TEXT_X + (BAR_WIDTH + GAP) * i, TEXT_Y);
-      ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -(times[i] * MAX_BAR_HEIGHT / maxTime));
-      ctx.fillText(Math.round(times[i]), TEXT_X + (BAR_WIDTH + GAP) * i, (BAR_Y - CLOUD_GAP - (times[i] * MAX_BAR_HEIGHT / maxTime)));
-    }
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], TEXT_X + (BAR_WIDTH + GAP) * i, TEXT_Y);
+    ctx.fillText(Math.round(times[i]), TEXT_X + (BAR_WIDTH + GAP) * i, (BAR_Y - CLOUD_GAP - (times[i] * MAX_BAR_HEIGHT / maxTime)));
+    ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : randomColor;
+    ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -(times[i] * MAX_BAR_HEIGHT / maxTime));
   }
 };
